9922877c9b48252ad0d016116a6c3e321392852e
Ívar Örn Halldórsson <ivar07@ru.is>
2013-01-28 18:43:08 +0000

    added clear and selector buttons

--- MvcApplication1/MvcApplication1/Scripts/whiteboard.js
+++ MvcApplication1/MvcApplication1/Scripts/whiteboard.js
@@ -4,13 +4,33 @@ $(document).ready(function () {
     var canvas = document.getElementById("imageView");
     var context = canvas.getContext("2d");
 
+    var tool = rect;
+
+    
+
+    
+    $("#rect").click(function () {
+
+        tool = "rect"
+         
+
+    });
+
+
     $("#imageView").mousedown(function (e) {
-        var x = e.pageX - this.offsetLeft;
-        var y = e.pageY - this.offsetTop;
+        if( tool == "rect"){
+            
+            var x = e.pageX - this.offsetLeft;
+            var y = e.pageY - this.offsetTop;
 
-        context.fillStyle = "blue";
-        context.fillRect(x, y, 50, 50);
+            context.fillStyle = "blue";
+            context.fillRect(x, y, 50, 50);
+        }
+    });
 
+    $ ("#clear").click(function () {
+        console.log("bla");
+        context.clearRect(0, 0, 900, 600);
     });
 
 });
