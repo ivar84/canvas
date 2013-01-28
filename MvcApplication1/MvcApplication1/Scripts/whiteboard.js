a7dccb4f81c64a66a5504ee42c66b7d5eb0a5690
Heimir Már Helgason <heimirmar@gmail.com>
2013-01-28 16:46:00 +0000

    bla

---
+++ MvcApplication1/MvcApplication1/Scripts/whiteboard.js
@@ -0,0 +1,16 @@
+﻿
+$(document).ready(function () {
+
+    var canvas = document.getElementById("imageView");
+    var context = canvas.getContext("2d");
+
+    $("#imageView").mousedown(function (e) {
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+
+        context.fillStyle = "blue";
+        context.fillRect(x, y, 50, 50);
+
+    });
+
+});
